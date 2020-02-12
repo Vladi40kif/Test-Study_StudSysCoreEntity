600e69d9c6cbd06e3538870d47f02567d8137ab1
Vladi40kif <tymoshchuk.if@gmail.com>
2020-02-12 16:01:57 +0200

    simple change walking skeleton for weasthercast

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+
+  	responce: any;
+
+  	constructor(private http: HttpClient) {
+    	this.http.get("https://localhost:44382/weatherforecast")
+    		.subscribe(respounse => {
+    			this.responce = respounse;
+    			console.log(this.responce);
+    		})
+  	};
+
+
+  	do():string {
+  		var data: string;
+
+  		// var i: number = 0;
+  		// while(this.responce[i]!=null){
+  		// 	data+= "date" + this.responce[i].date + "<br />";
+  		// 	data+= "temperatureC" + this.responce[i].temperatureC + "<br />";
+  		// 	data+= "summary" + this.responce[i].summary + "<br />";
+  		// 	data+= "<hr />";
+  		// }
+  		return data;
+  	};
+
 }
